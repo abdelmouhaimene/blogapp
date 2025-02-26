86a69d23ea627c800223a9741f779bbdec30c870
abdelmouhaimene <mimou4ever1999@gmail.com>
2025-02-26 15:24:38 +0000

    add fonts and global css and tailwind configs and finish Navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 export default function layout ({children} : Readonly<{children : React.ReactNode}>)  {
   return (
-    <main className="font-normal">
+    <main className="bg-black text-white">
         <Navbar />
         {children}
     </main>
